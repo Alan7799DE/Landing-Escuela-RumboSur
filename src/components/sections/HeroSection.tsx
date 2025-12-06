cf2be701b87b013c61324014aebafad380a6aafa
Alan7799DE <atautomatizaciones@gmail.com>
2025-12-06 19:08:43 -0300

    Update HeroSection.tsx

--- src/components/sections/HeroSection.tsx
+++ src/components/sections/HeroSection.tsx
@@ -24,7 +24,7 @@ export const HeroSection = () => {
 
       {/* Content */}
       <div className="relative z-10 container mx-auto px-4 py-12 md:py-8 text-center">
-        <div className="max-w-4xl mx-auto space-y-4 md:space-y-6 animate-fade-in">
+        <div className="max-w-4xl mx-auto space-y-2 md:space-y-6 animate-fade-in">
           <div className="flex justify-center mb-0">
             <img src={logoRumboSur} alt="Logo Rumbo Sur" className="w-48 md:w-60 h-auto" />
           </div>
