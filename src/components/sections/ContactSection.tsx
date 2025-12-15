05a9f117f4d24b7e9f4f98b1d2a675beaf4f8a31
Alan7799DE <atautomatizaciones@gmail.com>
2025-12-15 20:57:16 -0300

    Update WhatsApp link with a predefined message

--- src/components/sections/ContactSection.tsx
+++ src/components/sections/ContactSection.tsx
@@ -3,7 +3,7 @@ import { MapPin, MessageCircle } from "lucide-react";
 
 export const ContactSection = () => {
   const openWhatsApp = () => {
-    window.open("https://api.whatsapp.com/send/?phone=5491136135864&text&type=phone_number&app_absent=0", "_blank");
+    window.open("https://api.whatsapp.com/send/?phone=5491136135864&text=Hola%21%20Estuve%20en%20la%20p%C3%A1gina%20y%20me%20gustar%C3%ADa%20recibir%20m%C3%A1s%20informaci%C3%B3n%20acerca%20de%20las%20clases%20de%20canto....&type=phone_number&app_absent=0", "_blank");
   };
 
   return (
